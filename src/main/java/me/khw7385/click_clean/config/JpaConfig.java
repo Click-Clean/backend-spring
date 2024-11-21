1f94040265a93b48039d2632c4dafb72848ff855
hyunwon <khw73850@gmail.com>
2024-11-21 23:44:39 +0900

    feat: 생성 시간이 저장이 안되는 문제 해결

---
+++ src/main/java/me/khw7385/click_clean/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package me.khw7385.click_clean.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/me/khw7385/click_clean/domain/BaseEntity.java
+++ src/main/java/me/khw7385/click_clean/domain/BaseEntity.java
@@ -1,14 +1,17 @@
 package me.khw7385.click_clean.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate
     @Column(updatable=false)
