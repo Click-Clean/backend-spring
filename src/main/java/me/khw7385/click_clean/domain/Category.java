11820d140f044b8dbbb7a1d52e149baba2c86ec7
hyunwon <khw73850@gmail.com>
2024-11-21 14:48:44 +0900

    chore: 카테고리 수정

--- src/main/java/me/khw7385/click_clean/domain/Category.java
+++ src/main/java/me/khw7385/click_clean/domain/Category.java
@@ -1,5 +1,19 @@
 package me.khw7385.click_clean.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    SOCIAL, ECONOMY
+    POLITICS("정치"),
+    ECONOMY("경제"),
+    SOCIETY("사회"),
+    LIFE_CULTURE("생활/문화"),
+    IT_SCIENCE("IT/과학"),
+    WORLD("세계");
+
+    private String koreanName;
+
+    Category(String koreanName) {
+        this.koreanName = koreanName;
+    }
 }
