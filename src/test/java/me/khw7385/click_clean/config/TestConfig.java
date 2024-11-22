e7afc5e826918e3fc3bec670d58847e6ef0b9855
hyunwon <khw73850@gmail.com>
2024-11-22 15:47:29 +0900

    test: 키워드에 따라 기사 조회 테스트

--- src/test/java/me/khw7385/click_clean/config/TestConfig.java
+++ src/test/java/me/khw7385/click_clean/config/TestConfig.java
@@ -1,11 +1,15 @@
 package me.khw7385.click_clean.config;
 
 import jakarta.persistence.EntityManager;
+import me.khw7385.click_clean.repository.ArticleRepository;
+import me.khw7385.click_clean.repository.ArticleRepositoryTest;
 import me.khw7385.click_clean.repository.ScrapRepository;
 import me.khw7385.click_clean.repository.VoteRepository;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @TestConfiguration
 public class TestConfig {
     @Bean
@@ -14,4 +18,6 @@ public class TestConfig {
     }
     @Bean
     public VoteRepository voteRepository(EntityManager entityManager){return new VoteRepository(entityManager);}
+    @Bean
+    public ArticleRepository articleRepository(EntityManager entityManager){return new ArticleRepository(entityManager);}
 }
---
+++ src/test/java/me/khw7385/click_clean/repository/ArticleRepositoryTest.java
@@ -0,0 +1,67 @@
+package me.khw7385.click_clean.repository;
+
+import me.khw7385.click_clean.config.TestConfig;
+import me.khw7385.click_clean.domain.Article;
+import me.khw7385.click_clean.domain.Category;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@Import(TestConfig.class)
+public class ArticleRepositoryTest {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    @DisplayName("기사 제목과 본문에서 검색 키워드가 존재하는 기사 리스트 반환 테스트")
+    void findArticlesByKeywordOrderByCreatedAtDesc(){
+        // given
+        List<Article> articles = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++){
+            Article article = Article.builder()
+                    .title("Test title " + i + (i % 2 == 0 ? " keyword" : ""))
+                    .body("Test body " + i + (i % 3 == 0 ? " keyword" : ""))
+                    .url("test.com")
+                    .media("media")
+                    .summary("summary")
+                    .author("author")
+                    .probability(BigDecimal.valueOf(75.4))
+                    .category(Category.SOCIETY)
+                    .build();
+            articles.add(article);
+            em.persist(article);
+        }
+
+        // when
+        List<Article> foundArticles
+                = articleRepository.findArticlesByKeywordOrderByCreatedAtDesc("keyword", 0, 10);
+        // then
+        assertThat(foundArticles)
+                .isNotNull()
+                .hasSize(10)
+                .allSatisfy(article ->
+                    assertThat(article.getTitle().contains("keyword") || article.getBody().contains("keyword"))
+                            .isTrue()
+                );
+
+        assertThat(foundArticles)
+                .extracting(Article::getCreatedAt)
+                .isSortedAccordingTo(Comparator.reverseOrder());
+    }
+}
